32c00184c1154cf507ecd41f0487dec1028a55f2
Evgeni.ok14 <evgeni.ok@mail.ru>
2022-12-06 22:31:07 +0300

    added a new project: tusk19

---
+++ tusk19/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+Console.WriteLine("input five-digit number n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+    String s = n.ToString();  
+       while (n < 10000 || n > 100000)
+          {
+        Console.WriteLine("You entered not five-digit number!\nInput five-digit number n: "  );
+          n = Convert.ToInt32(Console.ReadLine());
+          }
+           if (s[0] == s[4] && s[1] == s[3])
+          {
+    Console.WriteLine("YES! This number is a mirror reflection of itself ");
+          }
+          else
+          {
+    Console.WriteLine("NO! This number isn't a mirror reflection of itself ");
+          
+          }
