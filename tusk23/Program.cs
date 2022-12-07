b813bf8809e8031d784dcb4391c63b90cd86dfa0
Evgeni.ok14 <evgeni.ok@mail.ru>
2022-12-07 11:31:24 +0300

    added a new project: tusk23

---
+++ tusk23/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+Console.WriteLine("Input any positive number n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= n; i++)
+ { 
+    double a = Math.Pow(i, 3);
+    Console.WriteLine(a);
+ }
