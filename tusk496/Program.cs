82cfb421d18e12ac3bceece1ee531fcb4220055b
Evgeni.ok14 <evgeni.ok@mail.ru>
2022-12-08 21:32:55 +0300

    added a new project: tusk496

---
+++ tusk496/Program.cs
@@ -0,0 +1,26 @@
+﻿
+    Console.WriteLine("Задайте длину массива n: ");
+    int n = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("длина массива равна:" + n);
+     int [] array = new int[n];
+    
+    for (int i = 3; i < n; i++)
+      {
+        array[i] = i; 
+        Console.Write(array[i] + " ");
+      }
+       int Sum = 0;
+      int max =array[0];
+      for (int i = 1; i < n -1; i++)
+      {
+         Sum = array[i-1] + array[i] + array[i+1]; //сумма 3 кустов
+         if (Sum > max)
+          {
+            max = Sum;
+          }
+      }
+          Console.Write("максимальное количество ягод равно: " + max);
+      
+
+
+        
